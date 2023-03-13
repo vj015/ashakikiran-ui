b16c2db634cab23963e236198480e788d5c889b6
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-03-13 12:06:31 +0530

    Added and installed fontawesome icons

--- ashakikiran-ui/src/main.js
+++ ashakikiran-ui/src/main.js
@@ -7,6 +7,20 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUserSecret);
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
