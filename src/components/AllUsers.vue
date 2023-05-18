6b6a564d56133ba1be2b3ab5926329429ea93f31
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-05-18 23:24:32 +0530

    add

---
+++ src/components/AllUsers.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ "This component will show all users in table" }}
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      items: [
+        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
+        { age: 21, first_name: "Larsen", last_name: "Shaw" },
+        { age: 89, first_name: "Geneva", last_name: "Wilson" },
+        { age: 38, first_name: "Jami", last_name: "Carney" },
+      ],
+    };
+  },
+  components: {},
+  methods: {},
+};
+</script>
+<style scoped></style>
